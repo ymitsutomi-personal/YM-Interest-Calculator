e3461d347d68c171614b2a1d8b6421967a5bfd6f
Yoshihiro Mitsutomi <hairy_hg99@hotmail.co.jp>
2021-02-14 14:24:18 +0000

    updated for the exercise

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+
+    if ((principal == "") || (principal == 0)){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+
+    var today = new Date();
+    var futureYear = today.getFullYear() + parseInt(years);
+
     
+    document.getElementById("result").innerHTML = 
+        "If you deposit " + principal + ","
+        + "<br/>at an interest rate of " + rate + "%."
+        + "<br/>You will receive an amount of " + interest + ","
+        + "<br/>in the year " + futureYear;
+}
+
+function showRate()
+{
+    var rate = document.getElementById("rate").value;
+    document.getElementById("labelRate").innerHTML = rate + " %";
 }
         
